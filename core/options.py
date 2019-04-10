1e7656243449d08de45803e2fe8915cfdc2ed397
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-04-10 22:26:37 +0200

    Create options.py

---
+++ core/options.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+
+from optparse import *
+from threading import *
+
+parser = OptionParser()
+parser.add_option("-u", "--user", dest="user", help="Enter the user.")
+parser.add_option("-l", "--lang", dest="lang", action="store_true", help="See the user's language.")
+parser.add_option("-c", "--chatbox", dest="chatbox", action="store_true", help="See the number of chatboxes posted.")
+parser.add_option("-s", "--status", dest="status", action="store_true", help="See the status of the user")
+parser.add_option("-p", "--point", dest="point", action="store_true")
+parser.add_option("-C", "--Challenge", dest="Challenge")
+parser.add_option("-m", "--machines", dest="machines")
+(options, args) = parser.parse_args()
+
+PollersUser   = options.user
+PollersLang   = options.lang
+PollersChat   = options.chatbox
+PollersStatus = options.status
+PollersPoint  = options.point
+PollersMachines  = options.machines
+PollersChall  = options.Challenge
