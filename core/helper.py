7d74462584c9ad5e797e791aeed3fcf74055204f
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-04-11 22:47:54 +0200

    Create helper.py

---
+++ core/helper.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+
+def help():
+
+    print "[Informations of username] :"
+    print "\t[-u/--user] | Username."
+    print "\t[-l/--lang] | See the language."
+    print "\t[-c/--chatbox] | See the numbers of chatboxes posted."
+    print "\t[-s/--status] | See the status of the user."
+    print "\t[-p/--point] | See the point of the user.\n"
+
+    print "[Challenge] :"
+    print "\t[-C/--Challenge] | See the challenge owned or not."
+    print "\t\t> App-Script"
+    print "\t\t> App-System"
+    print "\t\t> Cracking"
+    print "\t\t> Cryptanalysis"
+    print "\t\t> Forensic"
+    print "\t\t> Programming"
+    print "\t\t> Realist"
+    print "\t\t> Network"
+    print "\t\t> Steganography"
+    print "\t\t> Web-Client"
+    print "\t\t> Web-Server\n"
+
+if __name__ == "__main__":
+    help()
